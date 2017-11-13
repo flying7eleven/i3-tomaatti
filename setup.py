e96ade4c3ce9e0309f41c87088e61d520b03f36d
Tim Hütz <tim@huetz.biz>
2017-11-13 07:53:58 +0100

    Add some missing setup parameters

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
 	name='Tomaatti for i3',
 	version='0.1',
 	description='Pomodoro timer with i3 integration',
+	long_description='Pomodoro timer with i3 integration',
 	url='https://github.com/thuetz/i3-tomaatti',
 	author='Tim Hütz',
 	author_email='tim@huetz.biz',
 	license='GPLv3',
-	packages=['i3-tomaatti'],
+	packages=find_packages(),
 	install_requires=[],
-	zip_safe=False
+	zip_safe=False,
+	platforms=['linux']
 )
