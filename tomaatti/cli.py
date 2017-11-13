234187da8cef3bbd1512582941ca1d19a976a5a9
Tim Hütz <tim@huetz.biz>
2017-11-13 20:35:02 +0100

    Fix Python style errors according to PEP

--- tomaatti/cli.py
+++ tomaatti/cli.py
@@ -3,15 +3,15 @@ def script_entry_point():
 	from tomaatti import I3Integration
 
 	# define the actual argument parser
-	argumentParser = ArgumentParser(description='Pomodoro timer for i3')
-	argumentParser.add_argument('-w', '--working-period', type=int, default=25, help='The time you want to work on a project (in minutes).')
-	argumentParser.add_argument('-p', '--pause-period', type=int, default=5, help='The time you want to pause between to working periodes (in minutes).')
-	argumentParser.add_argument('-v', '--verbose', help='Increase output verbosity', action='store_true')
+	argument_parser = ArgumentParser(description='Pomodoro timer for i3')
+	argument_parser.add_argument('-w', '--working-period', type=int, default=25, help='The time you want to work on a project (in minutes).')
+	argument_parser.add_argument('-p', '--pause-period', type=int, default=5, help='The time you want to pause between to working periodes (in minutes).')
+	argument_parser.add_argument('-v', '--verbose', help='Increase output verbosity', action='store_true')
 
 	# now we can start parsing the supplied arguments
-	parsedArguments = argumentParser.parse_args()
+	parsed_arguments = argument_parser.parse_args()
 
-	print('i3 %s', I3Integration.get_block_name())
+	print('i3', I3Integration.get_block_name())
 
 
 if __name__ == '__main__':
