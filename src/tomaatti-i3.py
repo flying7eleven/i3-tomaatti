534e15abc16e2e7ab318dc3fbe7941330dad98ab
Tim Hütz <tim@huetz.biz>
2017-11-13 07:48:18 +0100

    Add the first few arguments to the argument parser

--- src/tomaatti-i3.py
+++ src/tomaatti-i3.py
@@ -1,2 +1,11 @@
 if __name__ == '__main__':
-	pass
+	from argparse import ArgumentParser
+
+	# define the actual argument parser
+	argumentParser = ArgumentParser(description='Pomodoro timer for i3')
+	argumentParser.add_argument('-w', '--working-period', type=int, default=25, help='The time you want to work on a project (in minutes).')
+	argumentParser.add_argument('-p', '--pause-period', type=int, default=5, help='The time you want to pause between to working periodes (in minutes).')
+	argumentParser.add_argument('-v', '--verbose', help='Increase output verbosity', action='store_true')
+
+	# now we can start parsing the supplied arguments
+	parsedArguments = argumentParser.parse_args()
