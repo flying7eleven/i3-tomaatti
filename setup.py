b89a9e7ae3f91620cca84f99082fb27abae338de
Tim Hütz <tim@huetz.biz>
2017-11-13 07:29:58 +0100

    Add the initial setup descriptor for the application

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+	name='Tomaatti for i3',
+	version='0.1',
+	description='Pomodoro timer with i3 integration',
+	url='https://github.com/thuetz/i3-tomaatti',
+	author='Tim Hütz',
+	author_email='tim@huetz.biz',
+	license='GPLv3',
+	packages=['i3-tomaatti'],
+	install_requires=[
+		'argparse',
+	],
+	zip_safe=False
+)
