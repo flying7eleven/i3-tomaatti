cb8d2a77a124b2aec03d6a7484ee37510c8688ac
Tim Hütz <tim@huetz.biz>
2017-11-14 08:34:40 +0100

    Add missing license information

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
+# Copyright (C) 2017 Tim Hütz
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program. If not,
+# see <http://www.gnu.org/licenses/>.
+
 from setuptools import setup, find_packages
 
 setup(
@@ -8,7 +18,7 @@ setup(
 	url='https://github.com/thuetz/i3-tomaatti',
 	author='Tim Hütz',
 	author_email='tim@huetz.biz',
-	license='GPLv3',
+	license='GPL-3.0',
 	packages=find_packages(),
 	install_requires=[],
 	zip_safe=False,
