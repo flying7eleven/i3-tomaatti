891ec3960904d2fbb44f9937d4bff0600b134c98
Tim Hütz <tim@huetz.biz>
2017-11-15 07:19:08 +0100

    Remove the temporary arguments from the command line interface

--- tomaatti/cli.py
+++ tomaatti/cli.py
@@ -14,9 +14,6 @@ def script_entry_point():
 
 	# define the actual argument parser
 	argument_parser = ArgumentParser(description='Pomodoro timer for i3')
-	argument_parser.add_argument('-w', '--working-period', type=int, default=25, help='The time you want to work on a project (in minutes).')
-	argument_parser.add_argument('-p', '--pause-period', type=int, default=5, help='The time you want to pause between to working periods (in minutes).')
-	argument_parser.add_argument('-v', '--verbose', help='Increase output verbosity', action='store_true')
 
 	# now we can start parsing the supplied arguments
 	parsed_arguments = argument_parser.parse_args()
