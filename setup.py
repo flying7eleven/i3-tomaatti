dc8096e23643de1694324051f20f62403b6cde0e
Tim Hütz <tim@huetz.biz>
2017-11-17 09:24:15 +0100

    Update the version number

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='tomaatti',
-	version='0.2.0',
+	version='0.3.0.dev1',
 	description='Pomodoro timer with i3 integration',
 	long_description='Pomodoro timer with i3 integration',
 	url='https://github.com/thuetz/i3-tomaatti',
