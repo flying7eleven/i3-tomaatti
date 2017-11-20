bee36053e17fd2c4efc10e6564239d723816eec1
Tim Hütz <tim@huetz.biz>
2017-11-20 12:45:57 +0100

    Ensure import * will work

--- tomaatti/__init__.py
+++ tomaatti/__init__.py
@@ -8,6 +8,14 @@
 # You should have received a copy of the GNU General Public License along with this program. If not,
 # see <http://www.gnu.org/licenses/>.
 
+__all__ = [
+	'TimerType',
+	'I3ButtonIdentifier',
+	'I3Integration',
+	'ConfigHelper',
+	'Tomaatti',
+]
+
 from .internal.timertype import TimerType
 from .internal.i3buttonidentifier import I3ButtonIdentifier
 from .internal.i3integration import I3Integration
