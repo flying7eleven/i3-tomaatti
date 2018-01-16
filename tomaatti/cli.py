50b060d4d2352d60bf2f63ce94ff2db058411d3d
Tim Hütz <tim@huetz.biz>
2018-01-16 12:33:23 +0100

    Update the copyright information

--- tomaatti/cli.py
+++ tomaatti/cli.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2017 Tim Hütz
+# Copyright (C) 2017 - 2018 Tim Hütz
 # This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
 # License as published by the Free Software Foundation, version.
 #
