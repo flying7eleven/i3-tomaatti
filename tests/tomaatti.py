c79ab353d08ea78808762350a5e93972581af106
Tim Hütz <tim@huetz.biz>
2018-01-26 07:43:38 +0100

    Add a missing encoding specifier for UTF-8

--- tests/tomaatti.py
+++ tests/tomaatti.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright (C) 2017 - 2018 Tim Hütz
 # This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
 # License as published by the Free Software Foundation, version.
