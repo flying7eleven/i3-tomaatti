8d31b2ef56aed82caa9e04d6cd81ae05bea0f402
Tim Hütz <tim@huetz.biz>
2018-01-29 07:18:36 +0100

    Change the way the tests are started

--- runTests.sh
+++ runTests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 PYTHONPATH=`pwd`:`pwd`/tomaatti:$PYTHONPATH
 echo "PYTHONPATH: $PYTHONPATH"
-python3 -m unittest discover -s ./tests -v -p '*.py'
+#python3 -m unittest discover -s ./tests -v -p '*.py'
+python3 setup.py test
