44013646cca9ebc5d6d926d5339f6eccc6c606b2
Tim Hütz <tim@huetz.biz>
2018-10-08 09:28:37 +0200

    chore: add a file to manage the configuration

---
+++ tomaatti/internal/configuration.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2017 - 2018 Tim Hütz
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program. If not,
+# see <http://www.gnu.org/licenses/>.
+
+class Configuratioj(object):
+    pass
