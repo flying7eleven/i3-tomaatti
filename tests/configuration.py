1d072eaa218929d90a4152b71637796ed6773308
Tim Hütz <tim@huetz.biz>
2018-10-09 16:03:31 +0200

    chore: start to implement a test for the configuration class

---
+++ tests/configuration.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2017 - 2018 Tim Hütz
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program. If not,
+# see <http://www.gnu.org/licenses/>.
+
+from tomaatti.internal.configuration import Configuration
+from unittest import TestCase
+
+
+class ConfigurationTest(TestCase):
+	def testTodo(self):
+		foo = Configuration()
+		self.assertTrue(True)
