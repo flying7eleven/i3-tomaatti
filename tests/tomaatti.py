7ffbbd6ab8e89429e0ee823fdb257b96870c0303
Tim Hütz <tim@huetz.biz>
2018-10-10 08:29:57 +0200

    chore: remove the test code which is not required anymore

--- tests/tomaatti.py
+++ tests/tomaatti.py
@@ -9,42 +9,8 @@
 # You should have received a copy of the GNU General Public License along with this program. If not,
 # see <http://www.gnu.org/licenses/>.
 
-from os.path import expanduser, join
 from unittest import TestCase
-from unittest.mock import patch, MagicMock, call
-
-from tomaatti.internal.tomaatti import Tomaatti, TimerType
 
 
 class TomaattiTest(TestCase):
-	@patch('os.path.exists')
-	@patch('os.makedirs')
-	def testCreateFullConfigIfItDoesNotExists(self, makedirs_patch, patch_exists):
-		patch_exists.return_value = False
-		makedirs_patch.return_value = None
-
-		config_mock = MagicMock()
-
-		test_object = Tomaatti()
-		test_object.toggle_timer = MagicMock()
-		test_object._persist_current_state = MagicMock()
-		test_object.initialize(config_mock)
-
-		config_mock.add_section.assert_has_calls([call('timer'), call('ui'), call('periods'), call('experimental')], any_order=True)
-		config_mock.assert_has_calls([
-			call.set('timer', 'mode', str(TimerType.WORKING.value[0])),
-			call.set('periods', 'working', '25'),
-			call.set('periods', 'break', '5')],
-			any_order=True)
-
-	@patch('os.path.exists')
-	def testReadConfigIfItExists(self, patch_exists):
-		patch_exists.return_value = True
-
-		test_object = Tomaatti()
-		test_object._create_initial_config = MagicMock()
-		test_object.initialize(MagicMock())
-
-		test_object._create_initial_config.assert_not_called()
-		test_object._application_config.read.assert_called_once_with(
-			join(expanduser('~/.config/tomaatti'), 'application_state.ini'))
+	pass
